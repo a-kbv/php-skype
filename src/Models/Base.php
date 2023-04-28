ad73e90c9dd172b8c21e83fef59a87f723701831
Atanas Korabov <atanas.k@getlinked.io>
2023-04-28 16:33:42 +0300

    dynamic object

--- src/Models/Base.php
+++ src/Models/Base.php
@@ -19,31 +19,6 @@ abstract class Base
         $propertyName = get_class($object);
         $propertyName = substr($propertyName, strrpos($propertyName, '\\') + 1);
         $this->{$propertyName} = $object;
-
-        // Generate the getter method
-        $getterName = 'get' . ucfirst($propertyName);
-        if (!method_exists($this, $getterName)) {
-            $this->{$getterName} = function () use ($propertyName) {
-                return $this->{$propertyName};
-            };
-        }
-
-        // Return the property
-        return $this->{$propertyName};
-    }
-
-    /**
-     * @param string $method
-     * @param mixed $args
-     * @return mixed
-     */
-    public function __call($method, $args)
-    {
-        if (isset($this->{$method}) && is_callable($this->{$method})) {
-            $func = $this->{$method};
-            return call_user_func_array($func, $args);
-        }
-        throw new \Exception('Undefined method: ' . $method);
     }
 
     /**
