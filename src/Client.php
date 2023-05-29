de7ff505607e48dd95c8f4489fd331249c7c1957
Atanas Korabov <atanas.k@getlinked.io>
2023-05-29 16:35:31 +0300

    fix version

--- src/Client.php
+++ src/Client.php
@@ -862,6 +862,7 @@ final class Client implements ClientInterface
             $this->sessionManager->removeSession($this->getSession());
             $this->login($account);
         }
+        
         $responseData = [];
         $eventMessages = [];
         if (200 === $response->getStatusCode()) {
